a84d4e1c5dd2ab8219e6b4790cf2f2e5ce346a75
iamtuuuuuu <vuanhtu111127@gmail.com>
2021-03-13 09:48:14 +0700

    test3

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, '/resources/views'));
 //routes init
 route(app);
 
-                app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
